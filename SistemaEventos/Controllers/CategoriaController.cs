04e72797dbaf5cb74bd136b1f178d9593575ba1c
armandoC1 <hr24006@esfe.agape.edu.sv>
2024-07-18 13:30:52 -0600

    Paginad'o no jala

---
+++ SistemaEventos/Controllers/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaEventos.Models;
+using X.PagedList;
+
+namespace SistemaEventos.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ContextoDeDatos contextoDatos;
+
+        //esta es la inyeccion de contexo de datos
+        public CategoriaController(ContextoDeDatos contextoDatos)
+        {
+            this.contextoDatos = contextoDatos;
+        }
+        public async Task<IActionResult> Index(int? page)
+        {
+            int pageSize = 10; 
+            int pageNumber = (page ?? 1); 
+
+            var categorias = await contextoDatos.Categorias.OrderByDescending(c => c.Id).ToPagedListAsync(pageNumber, pageSize);
+            
+            return View(categorias);
+        }
+    }
+}
